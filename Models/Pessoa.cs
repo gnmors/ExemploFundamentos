e74be9205f16036e7b91e7a297b6b23629ccab63
Giovanni Morais <giovannimo1@yahoo.com>
2025-05-27 17:00:39 -0300

    Sintaxe e tipos de dados

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -9,6 +9,7 @@ namespace ExemploFundamentos.Models
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+        public string NomeRepresentanteLegalDaPessoaFisica { get; set; }
 
         public void Apresentar()
         {
--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
 ﻿using ExemploFundamentos.Models;
 
-Pessoa pessoa1 = new Pessoa();
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy  HH:mm"));
 
-pessoa1.Nome = "Giovanni";
-pessoa1.Idade = 21;
-pessoa1.Apresentar();
+
+
+
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "Giovanni";
+//pessoa1.Idade = 21;
+//pessoa1.Apresentar();
