1a744d1f71b1a3d78cc0a24d7bb0d8cbf7a10054
Giovanni Morais <giovannimo1@yahoo.com>
2025-06-02 16:43:00 -0300

    Operador condicional

--- Program.cs
+++ Program.cs
@@ -1,31 +1,24 @@
 ﻿using ExemploFundamentos.Models;
 
-DateTime dataAtual = DateTime.Now.AddDays(5);
-Console.WriteLine(dataAtual.ToString("dd/MM/yyyy  HH:mm"));
+int quantidadeEmEstoque = 3;
+int quantidadeCompra = 0;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
 
 
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda inválida.");
+}
 
-
-
-// string apresentacao = "Olá, seja bem vindo";
-
-// int quantidade = 1;
-
-// double altura = 1.80;
-
-// decimal preco = 1.80M;
-
-// bool condicao = true;
-
-// Console.WriteLine(apresentacao);
-// Console.WriteLine("Valor da variável quantidade: " + quantidade);
-// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-// Console.WriteLine("Valor da variável preco: " + preco);
-// Console.WriteLine("Valor da variável condicao: " + condicao);
-
-
-//Pessoa pessoa1 = new Pessoa();
-
-//pessoa1.Nome = "Giovanni";
-//pessoa1.Idade = 21;
-//pessoa1.Apresentar();
+else if (possivelVenda)
+{
+    Console.WriteLine("Venda realizada.");
+}
+else
+{
+    Console.WriteLine("Desculpe não temos a quantidade desejada em estoque");
+}
