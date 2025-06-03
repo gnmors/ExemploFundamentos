cfe7d7c9f918f88db2ee0e7432f46f99ce3bc557
Giovanni Morais <giovannimo1@yahoo.com>
2025-06-03 16:31:11 -0300

    Menu interativo

--- obj/Debug/net9.0/ExemploFundamentos.AssemblyInfo.cs
+++ obj/Debug/net9.0/ExemploFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd2121cb0192d65a99ee030242b5a53e7e5a7a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d17fe918c7ba35dd16324c2d1cbf9c357bba8fe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
