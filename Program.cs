2b9949ded038e4484d7d799dde28b57a7950913a
Giovanni Morais <giovannimo1@yahoo.com>
2025-06-03 15:13:26 -0300

    Incremento e Decremento

--- Program.cs
+++ Program.cs
@@ -1,16 +1,36 @@
 ﻿using ExemploFundamentos.Models;
 
 
-Calculadora calc = new Calculadora();
+// Calculadora calc = new Calculadora();
 
 // calc.Somar(10, 30);
 // calc.Subtrair(10, 50);
 // calc.Multiplicar(15, 45);
 // calc.Dividir(2, 2);
 // calc.Potencia(3, 3);
-calc.Seno(30);
-calc.Coseno(30);
-calc.Tangente(30);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+
+int numeroIncremento = 10;
+
+Console.WriteLine(numeroIncremento);
+
+Console.WriteLine("Incrementando o 10");
+//numero = numero + 1;
+numeroIncremento++;
+
+Console.WriteLine(numeroIncremento);
+
+int numeroDecremento = 20;
+Console.WriteLine(numeroDecremento);
+Console.WriteLine("Decrementando o 20");
+numeroDecremento--;
+
+Console.WriteLine(numeroDecremento);
+
+
+
 
 
 // bool choveu = false;
