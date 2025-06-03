3fdb52fda76ab0b4ce1be9a4739265df280063ed
Giovanni Morais <giovannimo1@yahoo.com>
2025-06-03 14:09:24 -0300

    Operadores Aritmeticos

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
 ﻿using ExemploFundamentos.Models;
 
-bool choveu = false;
-bool estaTarde = false;
 
-if (!choveu && !estaTarde)
-{
-    Console.WriteLine("Vou pedalar");
-}
-else
-{
-    Console.WriteLine("Vou pedalar outro dia");
-}
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15, 45);
+calc.Dividir(2, 2);
+
+
+
+// bool choveu = false;
+// bool estaTarde = false;
+
+// if (!choveu && !estaTarde)
+// {
+//     Console.WriteLine("Vou pedalar");
+// }
+// else
+// {
+//     Console.WriteLine("Vou pedalar outro dia");
+// }
 
 
 
