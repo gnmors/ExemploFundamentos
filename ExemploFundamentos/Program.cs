0716959dd7d8ced6b639f0736e7b40f1542bf29f
Giovanni Morais <giovannimo1@yahoo.com>
2025-06-05 16:31:29 -0300

    Redimensionando um Array

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -7,6 +7,9 @@ arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 arrayInteiros[3] = 1;
 
+
+Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
 Console.WriteLine("Percorrendo array com FOR");
 
 for (int contador = 0; contador < arrayInteiros.Length; contador++)
@@ -15,13 +18,16 @@ for (int contador = 0; contador < arrayInteiros.Length; contador++)
 }
 
 
-Console.WriteLine("Percorrendo o array com o FOREACH");
 
-int contadorForeach = 0;
-foreach (int valor in arrayInteiros)
-{
-    Console.WriteLine($"Posição Nº {contadorForeach} - { valor}");
-}
+
+
+// Console.WriteLine("Percorrendo o array com o FOREACH");
+
+// int contadorForeach = 0;
+// foreach (int valor in arrayInteiros)
+// {
+//     Console.WriteLine($"Posição Nº {contadorForeach} - { valor}");
+// }
 
 
 
